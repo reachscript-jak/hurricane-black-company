2f6e0472ec1357fa0844dc22879c9bb4fc93fc73
Takumi Okada <t_okada@freelance-jak.com>
2020-02-12 01:32:19 +0900

    localでの実装完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const onClickApi = () => {
+    axios
+      .get('http://localhost:8080/sample')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>たいふ〜んぶらっくかんぱに〜</p>
+        <button className="App-link" style={{ width: '200px', height: '80px' }} onClick={onClickApi}>
+          <span style={{ fontSize: '1.4rem' }}>API実行</span>
+        </button>
+        <p style={{ color: 'red' }}>(※)↑押したら「localhost:8080/sample」にリクエスト投げるようにしてる</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
