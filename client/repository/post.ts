343f2dc4f7937f538d96dc3a0c28c7efd715d030
Takumi Okada <t_okada@freelance-jak.com>
2020-02-23 06:25:31 +0900

    新規登録API繋ぎ込み

--- client/repository/post.ts
+++ client/repository/post.ts
@@ -18,6 +18,38 @@ export default {
       return {
         error: false,
         data: response.data,
+        errorMessages: null,
+      };
+    } else {
+      return {
+        error: true,
+        data: null,
+        errorMessages: ['システムエラーが発生しました。時間をおいて再度お試しください。'],
+      };
+    }
+  },
+  async registPost(title: string, body: string, name: string): Promise<SuccessResult<null> | ErrorResult> {
+    const response = await axiosInstance
+      .post(`/post`, {
+        title,
+        body,
+        name,
+      })
+      .catch((e: AxiosError) => {
+        if (e.isAxiosError) {
+          return {
+            error: true,
+            errorMessages: ['システムエラーが発生しました。時間をおいて再度お試しください。'],
+            status: 500,
+            data: null,
+          };
+        }
+      });
+    if (response?.status === 200) {
+      return {
+        error: false,
+        data: null,
+        errorMessages: null,
       };
     } else {
       return {
