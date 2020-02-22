faf011e1b49f8d165988b6d7c8056d336ce52947
Takumi Okada <t_okada@freelance-jak.com>
2020-02-23 08:08:30 +0900

    型修正

--- client/types/post.ts
+++ client/types/post.ts
@@ -2,6 +2,6 @@ export interface Post {
   id: number;
   title: string;
   body: string;
-  favorite_count: number;
-  comment_count: number;
+  favorites_count: number;
+  comments_count: number;
 }
