3cb6ec7947e3975eef2eeda1bb1b4cbc89354649
Takumi Okada <t_okada@freelance-jak.com>
2020-02-22 21:12:19 +0900

    トップページの基盤作成

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import axios from 'axios';
-import { Button } from 'semantic-ui-react';
-import { useHistory } from 'react-router-dom';
+import { Button, Image, Container, Segment, Tab } from 'semantic-ui-react';
 import styled from 'styled-components';
 
+import TabPopular from '../templates/TabPopular';
+import TabNew from '../templates/TabNew';
+
 const Home = () => {
-  const history = useHistory();
+  const panes = [
+    {
+      menuItem: '　　　　　人気順　　　　　',
+      render: () => (
+        <Tab.Pane key="tabPopular" attached={false}>
+          <TabPopular />
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: '　　　　　新着順　　　　　',
+      render: () => (
+        <Tab.Pane key="tabNew" attached={false}>
+          <TabNew />
+        </Tab.Pane>
+      ),
+    },
+  ];
 
   const onClickApi = () => {
     axios
@@ -13,19 +32,21 @@ const Home = () => {
       .then(res => console.log(res.data))
       .catch(err => console.log(err));
   };
-  const onClickToDetail = () => {
-    history.push('/detail');
-  };
 
   return (
     <SCcontainer>
-      <p>はりけ〜んぶらっくかんぱに〜〜</p>
+      <Container fluid style={{ backgroundColor: '#00796b' }}>
+        <Image src={`${process.env.PUBLIC_URL}/img/haribura.png`} centered size="big" />
+      </Container>
+      <SCbodyContainer>
+        <Segment raised textAlign="center">
+          <SCmenuContainer>
+            <Tab menu={{ pointing: true, secondary: true, color: 'teal' }} panes={panes} />
+          </SCmenuContainer>
+        </Segment>
+      </SCbodyContainer>
       <Button color="green" style={{ width: '200px', height: '80px' }} onClick={onClickApi}>
         <span style={{ fontSize: '1.4rem' }}>API実行</span>
-      </Button>
-      <p style={{ color: 'red' }}>(※)↑押たらリクエスト投げるようにしているよ</p>
-      <Button color="teal" onClick={onClickToDetail}>
-        詳細ページへ移動
       </Button>
     </SCcontainer>
   );
@@ -36,7 +57,21 @@ const SCcontainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  background-color: #cefff9;
+`;
+
+const SCbodyContainer = styled(Container)`
+  margin-top: 16px;
+  margin-bottom: 16px;
+`;
+
+const SCmenuContainer = styled.div`
+  .ui {
+    justify-content: center;
+    .active {
+      background-color: '#00796B' !important;
+    }
+  }
 `;
 
 export default Home;
---
+++ client/templates/TabNew.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { List } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
+const TabNew = () => {
+  const history = useHistory();
+
+  const onClickToDetail = () => {
+    history.push('/detail');
+  };
+
+  return (
+    <SCcontainer>
+      <List divided relaxed animated>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item onClick={onClickToDetail}>
+          <List.Content>
+            <List.Header>ブラックストーリ新着順</List.Header>
+            <List.Description>ヒドイイネ　999　コメント　8888</List.Description>
+          </List.Content>
+        </List.Item>
+      </List>
+    </SCcontainer>
+  );
+};
+
+const SCcontainer = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export default TabNew;
