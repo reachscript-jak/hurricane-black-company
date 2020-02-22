223b06d28f4a1e1d182b9895d06b13ef61a3a1fa
Takumi Okada <t_okada@freelance-jak.com>
2020-02-23 05:28:43 +0900

    指定件数取得処理実装

--- client/components/pages/Home.tsx
+++ client/components/pages/Home.tsx
@@ -1,41 +1,47 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, Segment, Tab, Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
 import TabPopular from '../templates/TabPopular';
 import TabNew from '../templates/TabNew';
+import PostService from '../../repository/post';
 
 const Home = () => {
   const history = useHistory();
+
+  const [count, setCount] = useState(15);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const postFunc = async () => {
+      const res = await PostService.getPosts(count, '');
+      setData(res.data.posts);
+    };
+    postFunc();
+  }, [count]);
 
   const panes = [
     {
       menuItem: '　　　人気順　　　',
       render: () => (
-        <Tab.Pane key="tabPopular" attached={false}>
-          <TabPopular />
+        <Tab.Pane key="tabPopular" attached={false} loading={!data}>
+          <TabPopular data={data} />
         </Tab.Pane>
       ),
     },
     {
       menuItem: '　　　新着順　　　',
       render: () => (
-        <Tab.Pane key="tabNew" attached={false}>
-          <TabNew />
+        <Tab.Pane key="tabNew" attached={false} loading={!data}>
+          <TabNew data={data} />
         </Tab.Pane>
       ),
     },
   ];
 
-  const onClickApi = () => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/sample`)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err));
-  };
   const onClickToPost = () => history.push('/post');
+  const onClickCountButton = (num: number) => setCount(num);
 
   return (
     <>
@@ -51,11 +57,23 @@ const Home = () => {
           <SCmenuContainer>
             <Tab menu={{ pointing: true, secondary: true, color: 'teal' }} panes={panes} />
           </SCmenuContainer>
+          <SCbuttonContainer>
+            <Button.Group size="tiny">
+              <Button positive={count === 15} onClick={() => onClickCountButton(15)}>
+                15件表示
+              </Button>
+              <Button.Or />
+              <Button positive={count === 60} onClick={() => onClickCountButton(60)}>
+                60件表示
+              </Button>
+              <Button.Or />
+              <Button positive={count === 100} onClick={() => onClickCountButton(100)}>
+                100件表示
+              </Button>
+            </Button.Group>
+          </SCbuttonContainer>
         </Segment>
       </SCbodyContainer>
-      <Button color="green" style={{ width: '200px', height: '80px' }} onClick={onClickApi}>
-        <span style={{ fontSize: '1.4rem' }}>API実行</span>
-      </Button>
     </>
   );
 };
@@ -72,6 +90,10 @@ const SCmenuContainer = styled.div`
       background-color: '#00796B' !important;
     }
   }
+`;
+
+const SCbuttonContainer = styled.div`
+  margin-top: 16px;
 `;
 
 export default Home;
