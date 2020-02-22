6870177ac0ebb812369b33ed3dacde0b337f2c97
Takumi Okada <t_okada@freelance-jak.com>
2020-02-22 16:50:17 +0900

    フロントの不要ファイル整理

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
 
 const App = () => {
   const onClickApi = () => {
@@ -11,15 +9,12 @@ const App = () => {
       .catch(err => console.log(err));
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>はりけ〜んぶらっくかんぱに〜〜</p>
-        <button className="App-link" style={{ width: '200px', height: '80px' }} onClick={onClickApi}>
-          <span style={{ fontSize: '1.4rem' }}>API実行</span>
-        </button>
-        <p style={{ color: 'red' }}>(※)↑押したら「localhost:8080/api/sample」にリクエスト投げるようにしているよ</p>
-      </header>
+    <div>
+      <p>はりけ〜んぶらっくかんぱに〜〜</p>
+      <button style={{ width: '200px', height: '80px' }} onClick={onClickApi}>
+        <span style={{ fontSize: '1.4rem' }}>API実行</span>
+      </button>
+      <p style={{ color: 'red' }}>(※)↑押したらリクエスト投げるようにしているよ</p>
     </div>
   );
 };
