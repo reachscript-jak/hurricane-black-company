b25a3da29de9ffc37618e4a11b47821a34bbc4f0
m-shichida <42334697+m-shichida@users.noreply.github.com>
2020-03-22 23:32:54 +0900

    検索フロントモック作成 (#42)
    * 検索フロントモック作成
    
    * export defaultの位置修正

--- client/components/pages/Home.tsx
+++ client/components/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Button, Container, Segment, Tab, Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
+import SearchInput from '../molecules/SearchInput';
 import TabPopular from '../templates/TabPopular';
 import TabNew from '../templates/TabNew';
 
@@ -44,6 +45,7 @@ const Home = () => {
           <Button.Content hidden>(ｏﾟДﾟ)＝◯)`3゜)∵</Button.Content>
         </Button>
         <Segment raised textAlign="center">
+          <SearchInput />
           <SCmenuContainer>
             <Tab menu={{ pointing: true, secondary: true, color: 'teal' }} panes={panes} />
           </SCmenuContainer>
